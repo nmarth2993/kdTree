b4e1871c51e5702b9bbad401c258a9b334150702
nmarth2993 <nmarth2993@gmail.com>
2021-12-31 00:30:06 -0500

    Correct methods made static

--- src/NodeFactory.java
+++ src/NodeFactory.java
@@ -8,33 +8,37 @@ import java.util.Random;
 
 public class NodeFactory<T extends Number & Comparable<T>> {
 
+	public static final long SEED = 42;
+
 	Random randomGen;
 
 	public NodeFactory() {
-		randomGen = new Random(42);
+		randomGen = new Random(SEED);
 	}
 
 	public Node<T> createNode(T x, T y) {
 		return new Node<T>(x, y);
 	}
 
-	public Node<Integer> createRandIntegerNode() {
+	public static Node<Integer> createRandIntegerNode() {
 		int min = 0;
 		int max = 256;
 		return createRandIntegerNode(min, max);
 	}
 
-	public Node<Integer> createRandIntegerNode(int min, int max) {
+	public static Node<Integer> createRandIntegerNode(int min, int max) {
+		Random randomGen = new Random(SEED);
 		return new Node<Integer>(min + randomGen.nextInt(max + 1), min + randomGen.nextInt(max + 1));
 	}
 
-	public Node<Double> createRandDoubleNode() {
+	public static Node<Double> createRandDoubleNode() {
 		double min = 0;
 		double max = 256;
 		return createRandDoubleNode(min, max);
 	}
 
-	public Node<Double> createRandDoubleNode(double min, double max) {
+	public static Node<Double> createRandDoubleNode(double min, double max) {
+		Random randomGen = new Random(SEED);
 		return new Node<Double>(min + randomGen.nextDouble() * max, min + randomGen.nextDouble() * max);
 	}
 
