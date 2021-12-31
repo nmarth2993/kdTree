42abab69549d5094c72136077d0830a7979cd129
nmarth2993 <nmarth2993@gmail.com>
2021-12-31 00:47:00 -0500

    Add basic GUI class

---
+++ src/kdVisual.java
@@ -0,0 +1,31 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class kdVisual {
+	JFrame frame;
+	kdPanel panel;
+
+	public kdVisual(kdTree<Integer> tree) {
+		frame = new JFrame("kd tree visualization");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(700, 700));
+		// add insert/delete/search buttons? -- if I do this I need to add visual steps
+		panel = new kdPanel(tree);
+		frame.setContentPane(panel);
+
+		frame.setVisible(true);
+		frame.pack();
+
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			kdTree<Integer> tree = kdTreeFactory.createRandIntegerTree(10);
+			System.out.println("tree:");
+			tree.dump();
+			new kdVisual(tree);
+		});
+	}
+}
