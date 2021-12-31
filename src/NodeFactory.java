9ed2c2152abf71996583c605fe232b76d3d7c977
nmarth2993 <nmarth2993@gmail.com>
2021-12-31 00:44:27 -0500

    Make random class static
    To avoid same random numbers being generated every time (using set seed)

--- src/NodeFactory.java
+++ src/NodeFactory.java
@@ -10,11 +10,7 @@ public class NodeFactory<T extends Number & Comparable<T>> {
 
 	public static final long SEED = 42;
 
-	Random randomGen;
-
-	public NodeFactory() {
-		randomGen = new Random(SEED);
-	}
+	private static Random randomGen = new Random(SEED);
 
 	public Node<T> createNode(T x, T y) {
 		return new Node<T>(x, y);
@@ -27,7 +23,6 @@ public class NodeFactory<T extends Number & Comparable<T>> {
 	}
 
 	public static Node<Integer> createRandIntegerNode(int min, int max) {
-		Random randomGen = new Random(SEED);
 		return new Node<Integer>(min + randomGen.nextInt(max + 1), min + randomGen.nextInt(max + 1));
 	}
 
@@ -38,7 +33,6 @@ public class NodeFactory<T extends Number & Comparable<T>> {
 	}
 
 	public static Node<Double> createRandDoubleNode(double min, double max) {
-		Random randomGen = new Random(SEED);
 		return new Node<Double>(min + randomGen.nextDouble() * max, min + randomGen.nextDouble() * max);
 	}
 
